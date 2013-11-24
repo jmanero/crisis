7a16adb0e9827d1e69cacf915386db23d264427b
John Manero <jmanero@dyn.com>
2013-11-24 16:13:22 -0500

    Create service in cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# identity_service cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: John Manero (<jmanero@dyn.com>)
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: identity_service
+# Recipe:: default
+#
+# Copyright (C) 2013 John Manero
+# 
+# All rights reserved - Do Not Redistribute
+#
